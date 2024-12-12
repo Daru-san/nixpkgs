573676959567728ee1cb990422b736e7320e073a
Daru <mica.tui@proton.me>
2024-12-12 15:50:01 +0200

    nu_plugin_periodic_table: init at 0.2.9

---
+++ pkgs/shells/nushell/plugins/periodic-table.nix
@@ -0,0 +1,45 @@
+{
+  lib,
+  rustPlatform,
+  fetchFromGitHub,
+  stdenv,
+  nix-update-script,
+  IOKit,
+  CoreFoundation,
+}:
+
+rustPlatform.buildRustPackage rec {
+  pname = "nu_plugin_periodic_table";
+  version = "0.2.9";
+
+  src = fetchFromGitHub {
+    owner = "JosephTLyons";
+    repo = "nu_plugin_periodic_table";
+    tag = "v${version}";
+    hash = "sha256-zeErTvjR4sQ0nKSc3KpMMxHZ1kl7h8+suRVWkuiNYZ4=";
+  };
+
+  cargoHash = "sha256-6SoMhc1xRXdpzEM/LxagSqaoZhXy2GrkbPRvtd3ziNM=";
+
+  cargoBuildFlags = [ "--package nu_plugin_periodic_table" ];
+
+  nativeBuildInputs = lib.optionals stdenv.isDarwin [
+    rustPlatform.bindgenHook
+  ];
+
+  buildInputs = lib.optionals stdenv.isDarwin [
+    CoreFoundation
+    IOKit
+  ];
+
+  passthru.updateScript = nix-update-script { };
+
+  meta = {
+    description = "Periodic table plugin for nushell";
+    homepage = "https://github.com/JosephTLyons/nu_plugin_periodic_table";
+    changelog = "https://github.com/JosephTLyons/nu_plugin_periodic_table/blob/${src.tag}/CHANGELOG.md";
+    license = lib.licenses.gpl3Only;
+    maintainers = with lib.maintainers; [ daru-san ];
+    mainProgram = "nu_plugin_periodic_table";
+  };
+}
